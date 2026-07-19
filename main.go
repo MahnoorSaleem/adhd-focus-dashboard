946080677f78ade3918085dbf05a9a22b1e9d649
Mahnoor Saleem <mahnoor.saleem@quantumgate.ae>
2026-07-20 00:07:17 +0400

    Step 2: blocking fake AI call - shows the delay problem

--- main.go
+++ main.go
@@ -4,14 +4,22 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
 
 func dumpHandler(w http.ResponseWriter, r *http.Request) {
 	response, err := io.ReadAll(r.Body)
+	result := FakeAIProcess((string(response)))
 	if err != nil {
 		panic("something went wrong")
 	}
-	w.Write(response)
+	w.Write([]byte(result))
+}
+
+func FakeAIProcess(text string) string {
+	time.Sleep((3 * time.Second))
+	return "Task: " + text
+
 }
 
 func main() {
